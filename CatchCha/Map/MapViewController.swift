969cf1a9676930a2b3fe4f9b89618d3724426835
ryr0121 <ryr0121@gmail.com>
2022-11-13 05:24:27 +0900

    feature: generate Map VC

--- CatchCha/Map/MapViewController.swift
+++ CatchCha/Map/MapViewController.swift
@@ -1,29 +1,65 @@
-//
-//  MapViewController.swift
-//  CatchCha
-//
-//  Created by 정태우 on 2022/11/12.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var destinationInputView: UIView!
+    @IBOutlet weak var destinationTextField: UITextField!
+    @IBOutlet weak var destinationMapkit: MKMapView!
+    
+    
+    @IBAction func destinationTextFieldDidTap(_ sender: Any) {
+        // 키보드 안 보이게 하고, Destination VC로 연결
+    }
+        
 
-        // Do any additional setup after loading the view.
+    var locationManager: CLLocationManager = CLLocationManager() // location manager
+    var currentLocation: CLLocation! // 내 위치 저장
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       locationManager = manager
+       if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+           currentLocation = locationManager.location
+       }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setComponentsValue() {
+        self.destinationInputView.layer.cornerRadius = 30
+        self.destinationTextField.attributedPlaceholder = NSAttributedString(string: "어디로 갈까요?", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        
+        self.destinationInputView.layer.shadowColor = UIColor.black.cgColor
+        self.destinationInputView.layer.masksToBounds = false
+        self.destinationInputView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        self.destinationInputView.layer.shadowRadius = 8
+        self.destinationInputView.layer.shadowOpacity = 1.0
     }
-    */
+    
+    func setUserCurrentLocationInMapkit() {
+        // 사용자 현재 위치 보이기
+        self.destinationMapkit.showsUserLocation = true
+        self.destinationMapkit.setUserTrackingMode(.follow, animated: true)
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startMonitoringSignificantLocationChanges()
+        self.currentLocation = locationManager.location
+    }
+
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.destinationMapkit.mapType = MKMapType.standard
+
+        setComponentsValue()
+        setUserCurrentLocationInMapkit()
+    }
 
 }
+
+
+
